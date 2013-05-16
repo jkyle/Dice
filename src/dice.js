9ae052d45260320689f5352952ef72f7d6e3ad14
J. Kyle <jkyle@causethought.com>
2013-05-16 07:33:08 -0500

    Added some animation stuff. Mostly for experimentation.

--- src/dice.js
+++ src/dice.js
@@ -12,7 +12,8 @@ define(
 
     var Dice = function(faces){
       this.$el = $('<div>').addClass("die");
-
+      var that = this;
+      this.$el.click(function(e){ that.spin() })
       this.faces = faces;
       this.numberOfFaces = faces.length;
       this.activeFace = this.faces[random(this.numberOfFaces)];
@@ -22,6 +23,35 @@ define(
     Dice.prototype.roll = function(){
       this.rollCount += 1;
       this.activeFace = this.faces[random(this.numberOfFaces)];
+    }
+
+    Dice.prototype.spin = function(){
+      this.$el.addClass('rolling');
+      var start = new Date().getTime();
+      var last = start;
+      var that = this;
+
+      that.roll();
+      that.render();
+
+      var step = function(timestamp){
+        var now = new Date().getTime(),
+            ds = now - start,
+            dt = now - last;
+        if(dt > 50){
+          that.roll();
+          that.render();
+          last = now;
+        }
+        if(ds < 400){
+          requestAnimationFrame(step);
+        } else {
+          that.$el.removeClass('rolling');
+        }
+
+      }
+
+      step();
     }
 
     Dice.prototype.reset = function(){
